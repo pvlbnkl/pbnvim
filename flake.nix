729e3b784a81692c2ad2835dab92f16d8524db1c
Pavlo Bunakalia <pblo@tuta.io>
2024-03-04 17:49:45 -0500

    clean neovim binary

--- flake.nix
+++ flake.nix
@@ -2,14 +2,14 @@
   description = "personal neovim flake";
   inputs = {
     nixpkgs = {
-      url = "github:NixOS/nixpkgs";
-    };
-  };
+      url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    }; 
     neovim = {
       url = "github:neovim/neovim/stable?dir=contrib";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-  outputs = { self, nixpkgs, neovim }: { 
+  };
+  outputs = inputs @ { self, nixpkgs, neovim, }: { 
     packages.aarch64-darwin.default = neovim.packages.aarch64-darwin.neovim;
     apps.aarch64-darwin.default = {
       type = "app";
